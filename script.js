0a9ce59869df07f638de6df1b635e98d58194b44
Karina <karina.isl@inbox.ru>
2023-04-28 11:15:08 +0900

    add JS movies

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-console.log(1)
-const storeName = 'Karina';
-const storeDescription = {
-    budget: 10000,
-    employees: ['Alisa', 'Anna', 'Kate'],
-    products: {
-        dress: 150,
-        bread: 200
-    },
-    open: true,
-}
 
-const answers = [];
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваше фамилия?', '');
-answers[2] = prompt('Как ваше возраст?', ' ');
-
-document.write(answers)
-console.log(answers)
-
-for(var i=0; i < 3; i++){
-    setTimeout(function() {alert(i)}, 1000 + i)
-    }
+    const number0Films = +prompt('Сколько фильмов вы уже просмотрели?', '');
     
+    const personalMovieDB ={
+        count: number0Films,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    }; 
+    const answerOne = prompt('Один из последних просмотренных фильмов?', ''),
+     answerTwo = prompt('Насколько оцените его?', ''),
+     answerThree = prompt('Один из последних просмотренных фильмов?', ''),
+     answerFour = prompt('Насколько оцените его?', '');
+    
+  personalMovieDB.movies[answerOne] = answerTwo;
+  personalMovieDB.movies[answerThree] = answerFour;
+console.log(personalMovieDB)
+
+
